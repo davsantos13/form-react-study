6c89479051a4049086a1916b585f8029072744af
David Santos Evangelista <devangelista@grupocereal.local>
2021-06-24 16:26:02 -0300

    Finalizando formulário com formik

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+
+
+import schema from './schema';
 
 function App() {
+
+  function onSubmit(values, actions) {
+    console.log('Submiting...', values);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        validationSchema={schema}
+        onSubmit={onSubmit}
+        initialValues={{
+          name: '',
+          email: ''
+        }}
+        render={({ values, errors, touched, isValid }) => (
+          <Form>
+            <div>
+              <label>Nome</label>
+              <Field name="name" type="text" />
+              <ErrorMessage name="name" />
+
+            </div>
+            <div>
+              <label>Email</label>
+              <Field name="email" type="email" />
+              <ErrorMessage name="email" />
+
+            </div>
+            <button type="submit" disabled={!isValid}>Enviar</button>
+          </Form>
+        )}
+      >
+
+      </Formik>
     </div>
   );
 }

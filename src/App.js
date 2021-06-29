a1b8368573ac599893a895bd8293d9901c4c9f78
David Evangelista <daavidkarlos@gmail.com>
2021-06-29 08:45:01 -0300

    Trabalhando com request via cep

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import './App.css';
-import { Formik, Field, Form, ErrorMessage } from 'formik'
+import "./App.css";
+import { Formik, Field, Form } from "formik";
 
-
-import schema from './schema';
+import schema from "./schema";
 
 function App() {
-
   function onSubmit(values, actions) {
-    console.log('Submiting...', values);
+    console.log("Submiting...", values);
   }
 
+  function onBlurCep(event, setFieldValue) {
+    const { value } = event.target;
 
+    const cep = value?.replace(/[^0-9]/g, "");
+
+    if (cep?.length !== 8) {
+      return;
+    }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFieldValue("bairro", data.bairro);
+        setFieldValue("cidade", data.localidade);
+        setFieldValue("rua", data.logradouro);
+        setFieldValue("uf", data.uf);
+        setFieldValue("numero", data.numero);
+      });
+  }
 
   return (
     <div className="App">
@@ -18,29 +34,56 @@ function App() {
         validationSchema={schema}
         onSubmit={onSubmit}
         initialValues={{
-          name: '',
-          email: ''
+          cep: "",
+          rua: "",
+          numero: "",
+          bairro: "",
+          cidade: "",
+          uf: "",
         }}
-        render={({ values, errors, touched, isValid }) => (
+        render={({ isValid, setFieldValue, values }) => (
           <Form>
-            <div>
-              <label>Nome</label>
-              <Field name="name" type="text" />
-              <ErrorMessage name="name" />
-
+            <div className="form-control-group">
+              <label>CEP</label>
+              <Field
+                name="cep"
+                type="text"
+                onBlur={(ev) => onBlurCep(ev, setFieldValue)}
+              />
             </div>
-            <div>
-              <label>Email</label>
-              <Field name="email" type="email" />
-              <ErrorMessage name="email" />
 
+            <div className="form-control-group">
+              <label>RUA</label>
+              <Field name="rua" type="text" />
             </div>
-            <button type="submit" disabled={!isValid}>Enviar</button>
+
+            <div className="form-control-group">
+              <label>Número</label>
+              <Field name="numero" type="text" />
+            </div>
+
+            <div className="form-control-group">
+              <label>Bairro</label>
+              <Field name="bairro" type="text" />
+            </div>
+
+            <div className="form-control-group">
+              <label>Cidade</label>
+              <Field name="cidade" type="text" />
+            </div>
+
+            <div className="form-control-group">
+              <label>Estado</label>
+              <Field component="select" name="uf">
+                <option value={null}>Selecione o Estado</option>
+                <option value="SP">São Paulo</option>
+                <option value="GO">Goiás</option>
+              </Field>
+            </div>
+            <button type="submit">Enviar</button>
           </Form>
         )}
-      >
-
-      </Formik>
+      ></Formik>
     </div>
   );
 }
